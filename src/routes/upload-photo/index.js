0051754f8c5851334517085e00023a01d8eddec5
alaa nasser <alaanasser749@gmail.com>
2022-09-13 14:04:43 +0200

    layout-logout importriren

--- src/routes/upload-photo/index.js
+++ src/routes/upload-photo/index.js
@@ -1,25 +1,10 @@
 import React from "react";
 import "./index.scss";
-<<<<<<< Updated upstream
-import Layout from "../../Layout";
-
-export default function Upload() {
-  return (
-    <Layout>
-      <div className="Upload-Photo-Route">
-        <div className="wrapper">
-
-          
-        </div>
-      </div>
-    </Layout>
-=======
 import LayoutLogout from "../../Layout-Logout";
 export default function Upload() {
   return (
     <LayoutLogout>
       <div>Upload Routes</div>
     </LayoutLogout>
->>>>>>> Stashed changes
   );
 }
