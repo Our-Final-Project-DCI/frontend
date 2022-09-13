84022f8a60a6e53fcb29020270742d4d7467130f
Alexander Li <YouLo666@icloud.com>
2022-09-13 14:50:15 +0200

    update

---
+++ src/routes/update/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.scss";
+import Layout from "../../Layout";
+
+export default function Update() {
+  return (
+    <Layout>
+      <div>Update</div>
+    </Layout>
+  );
+}
