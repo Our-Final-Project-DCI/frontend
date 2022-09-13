6585c8debd4e797183c2abbfeaecc6e36944b2bb
alaa nasser <alaanasser749@gmail.com>
2022-09-13 11:37:05 +0200

    add mockups and import Layout Component

--- src/routes/upload-photo/index.js
+++ src/routes/upload-photo/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import './index.scss'
+import React from "react";
+import "./index.scss";
+import Layout from "../../Layout";
 
 export default function Upload() {
   return (
-    <div>Upload Routes</div>
-  )
+    <Layout>
+      <div className="Upload-Photo-Route">Upload Routes</div>
+    </Layout>
+  );
 }
