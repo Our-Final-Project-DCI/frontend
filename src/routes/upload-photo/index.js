d8b6999f07c3e0af2560a3efb21bece26c01f784
alaa nasser <alaanasser749@gmail.com>
2022-09-14 11:08:34 +0200

    react states definieren

--- src/routes/upload-photo/index.js
+++ src/routes/upload-photo/index.js
@@ -3,28 +3,43 @@ import "./index.scss";
 import LayoutLogout from "../../Layout-Logout";
 
 // images
-import uploadFile from "./images/uploadFile.png";
-import avatar from "./images/avatar.webp";
+import svg from "./images/uploadFile.png";
+
 export default function Upload() {
+  const [title, setTitle] = React.useState("");
+  const [location, setLocation] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [photoFile, setPhotoFile] = React.useState("");
+
+  const [isFetching, setIsFetching] = React.useState(false);
+  const [error, setError] = React.useState(false);
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
     <LayoutLogout>
       <div className="Upload-Photo-Route">
         <div className="wrapper">
-          <form action="" className="upload-photo-form">
+          <form className="upload-photo-form" onSubmit={submitHandler}>
             <div className="photo">
-              <img src={avatar} alt="" />
+              <img src={photoFile} alt="" />
             </div>
             <div className="input-group">
               <div className="choose-btn">
-                <button>choose file</button>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={(e) => setPhotoFile(e.target.files[0])}
+                />
               </div>
+
               {/* <div>
                 <input placeholder="Category" type="text" />
               </div> */}
               <div class="custom-select">
                 <select>
                   <option value="0">Category:</option>
-                  <option value="1">Natur</option>
+                  <option value="1">Nature</option>
                   <option value="2">Foods</option>
                   <option value="3">Cars</option>
                   <option value="4">Arts</option>
@@ -34,10 +49,20 @@ export default function Upload() {
               </div>
 
               <div>
-                <input placeholder="title" type="text" />
+                <input
+                  placeholder="title"
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
               </div>
               <div>
-                <input placeholder="location" type="text" />
+                <input
+                  placeholder="location"
+                  type="text"
+                  value={location}
+                  onChange={(e) => setLocation(e.target.value)}
+                />
               </div>
               <div>
                 <textarea
@@ -45,6 +70,8 @@ export default function Upload() {
                   name="textarea"
                   rows="8"
                   cols="22"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
                 />
               </div>
               {/* <div className="upload-btn">
@@ -56,7 +83,7 @@ export default function Upload() {
             <button type="submit">Upload</button>
           </div>
           <div className="svg">
-            <img src={uploadFile} alt="" />
+            <img src={svg} alt="" />
           </div>
         </div>
       </div>
