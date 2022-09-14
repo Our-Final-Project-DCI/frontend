90a0b92594c8877984970fed1bc853a4b2604195
alaa nasser <alaanasser749@gmail.com>
2022-09-15 01:36:38 +0200

    first push

--- src/routes/photos_id/index.js
+++ src/routes/photos_id/index.js
@@ -1,22 +1,21 @@
 import React from "react";
+import "./index.scss";
+import LayoutLogout from "../../Layout-Logout";
 
 // Images
-import "./index.scss";
 import avatar from "./images/avatar.webp";
 import f1 from "./images/f1.jpg";
-// Icons
+
 // Icons
 import { BiHeart } from "react-icons/bi";
-import { BiShareAlt } from "react-icons/bi";
-import { BiUserPlus } from "react-icons/bi";
 
 export default function Photo() {
   //const params = useParams();
   const [photo, setPhoto] = React.useState(null);
   const [comment, setComment] = React.useState("");
   return (
-    <div className="Photos-Id-Route">
-      <div className="wrapper">
+    <LayoutLogout>
+      <div className="Photos-Id-Route">
         <div className="user">
           <div className="avatar">
             <a href="/account">
@@ -25,51 +24,52 @@ export default function Photo() {
           </div>
           <h3>username</h3>
         </div>
-
-        <div className="user-actions">
-          <button className="download-btn">download</button>
-          <div className="likes">
-            <span className="total">
-              <em>200</em>
-            </span>
-            <span className="like-btn">
-              <a href="/">
-                <BiHeart />
-              </a>
-            </span>
-          </div>
-        </div>
-
-        <div className="user-photo">
-          <img src={f1} alt="" />
-        </div>
-
-        <section className="comment-section">
-          <form action="">
-            <div className="user-comment">
-              <div className="avatar">
-                <a href="/account">
-                  <img src={avatar} alt="" className="hover_opacity" />
+        <div className="wrapper">
+          <div className="user-actions">
+            <button className="download-btn">download</button>
+            <div className="likes">
+              <span className="total">
+                <em>200</em>
+              </span>
+              <span className="like-btn">
+                <a href="/">
+                  <BiHeart />
                 </a>
-              </div>
-              <input type="text" placeholder="Type comment hier..." />
-            </div>
-          </form>
-
-          <div className="user-comments">
-            <div className="user">
-              <img src={avatar} alt="" className="hover_opacity" />
-            </div>
-            <div className="comments">
-              <h4>User 1</h4>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo
-                beataee!
-              </p>
+              </span>
             </div>
           </div>
-        </section>
+
+          <div className="user-photo">
+            <img src={f1} alt="" />
+          </div>
+
+          <section className="comment-section">
+            <form action="">
+              <div className="user-comment">
+                <div className="avatar">
+                  <a href="/account">
+                    <img src={avatar} alt="" className="hover_opacity" />
+                  </a>
+                </div>
+                <input type="text" placeholder="Type comment hier..." />
+              </div>
+            </form>
+
+            <div className="user-comments">
+              <div className="user">
+                <img src={avatar} alt="" className="hover_opacity" />
+              </div>
+              <div className="comments">
+                <h4>User 1</h4>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo
+                  beataee!
+                </p>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
-    </div>
+    </LayoutLogout>
   );
 }
