9f9a1301b87387ff1b68499f622a034936da4e2a
alaa nasser <alaanasser749@gmail.com>
2022-09-15 10:21:29 +0200

    first commit

--- src/routes/update/index.js
+++ src/routes/update/index.js
@@ -1,31 +1,34 @@
 import React from "react";
 import "./index.scss";
-import Layout from "../../Layout";
-
+import LayoutLogout from "../../Layout-Logout";
 
 export default function Update() {
- 
-  const submitHander = (e) => {e.preventDefault(); console.log("submit")};
-  return (
-    <Layout> 
-    <form  className="updateform" onSubmit={submitHander}>
-      <select>
-        <option value="male">male</option>
-        <option value="female">female</option>
-      </select>
-      <input type="text" placeholder="Fullname" name='name' />
-      <input type="text" placeholder="username"  name='username'/>
-      <input type="text" placeholder="city"  name='city'/>
-      <input type="text" placeholder="Land" name='country'/>
-      <textarea  placeholder="about" />
-      <div className="social">
-      <a href="">Twiter</a>
-      <a href="">Facebook</a>
-      <a href="">Instagram</a>
-      </div>
+  const submitHander = (e) => {
+    e.preventDefault();
+  };
 
-      <button type="submit" >Update</button>
-    </form>
-    </Layout>
+  return (
+    <LayoutLogout>
+      <div className="Update-User-Profile">
+        <form className="updateform" onSubmit={submitHander}>
+          <select>
+            <option value="male">male</option>
+            <option value="female">female</option>
+          </select>
+          <input type="text" placeholder="Fullname" name="name" />
+          <input type="text" placeholder="username" name="username" />
+          <input type="text" placeholder="city" name="city" />
+          <input type="text" placeholder="Land" name="country" />
+          <textarea placeholder="about" />
+          <div className="social">
+            <a href="">Twiter</a>
+            <a href="">Facebook</a>
+            <a href="">Instagram</a>
+          </div>
+
+          <button type="submit">Update</button>
+        </form>
+      </div>
+    </LayoutLogout>
   );
 }
