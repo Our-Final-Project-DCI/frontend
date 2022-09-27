6bbe18acf3ba50197da900f52e85b3f1c7d48ec2
alaa nasser <alaanasser749@gmail.com>
2022-09-27 15:18:03 +0200

    line empty

--- src/routes/overview/index.js
+++ src/routes/overview/index.js
@@ -63,7 +63,6 @@ export default function Overview() {
                 )}
                 alt=""
               />
-
               <button className="like">
                 <FaRegHeart />
               </button>
