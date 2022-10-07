3bfa704f625da71fbf781b33f49c61807c152a3a
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-10-07 09:39:53 +0200

    changed styling of contact component and added formspree

--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -5,30 +5,36 @@ import Layout from "../../Layout";
 export default function Contact() {
   return (
     <Layout>
-      <div className="Contact">
+      <form action="https://formspree.io/f/mrgjzjea"
+           method="POST" 
+           className="Contact">
          <div className="contact-component">
             <h2>Get In Touch</h2>
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
             <label>Message</label>
-            <textarea rows={10} />
+            <textarea name="messge" required rows={10} />
             <div className="input-contact">
                 <div className="input-group">
                   <div className="label">Email Adress</div>
                   <input
+                    name="email"
                     type="text"
+                    required
                   />
                 </div>
 
                 <div className="input-group">
                   <div className="label">Fullname</div>
                   <input
+                    name="fullname"
                     type="text"
+                    required
                   />
                 </div>
               </div>
               <button>Send</button>
          </div>
-      </div>
+      </form>
     </Layout>
   );
 }
